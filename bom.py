d7ef87cfd2a77c853df2d0ae1cbe797b26fbda03
mintingg <minting063@gmail.com>
2025-02-04 10:54:55 +0800

    Update bom.py

--- bom.py
+++ bom.py
@@ -115,7 +115,7 @@ def math_bomb():
     time.sleep(1)
     print("Hint will be given after a wrong answer , but there will be one fake hint.")
     time.sleep(1)
-    print("🔥 Hit a streak of 3 correct answers and earn a 2x bonus")
+    print("🔥 Hit a streak of 3 correct answers and earn a 2x bonus !")
     time.sleep(1)
 
     #setting a initial value 
